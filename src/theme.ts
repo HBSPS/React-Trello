6db81f0ba2d92e9f71872b1e69acdb8d6d7a6dc3
HBSPS <wiyeong79@naver.com>
2022-08-26 00:09:50 +0900

    :tada: React - Trello Clone Start
    React - Trello Clone Start

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#2d3436",
+    textColor: "#ffeaa7",
+    accentColor: "#4ECDC4",
+    cardBgColor: "#636e72"
+    // #ffeaa7
+};
+
+export const lightTheme: DefaultTheme = {
+    bgColor: "#dfe6e9",
+    textColor: "#2c3e50",
+    accentColor: "#0984e3",
+    cardBgColor: "white"
+};
