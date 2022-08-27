a29a3271f89bf81d6f48ba29e4de5f4a4ec74af0
HBSPS <wiyeong79@naver.com>
2022-08-28 00:58:54 +0900

    :sparkles: Placeholder
    Placeholder를 사용해 보드의 크기가 변화하는 것을 방지

--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,9 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-    bgColor: "#2d3436",
-    textColor: "#ffeaa7",
-    accentColor: "#4ECDC4",
-    cardBgColor: "#636e72"
-    // #ffeaa7
-};
-
-export const lightTheme: DefaultTheme = {
-    bgColor: "#dfe6e9",
+export const trelloTheme: DefaultTheme = {
+    bgColor: "#3f8cf2",
+    boardColor: "#dfe6e9",
+    cardColor: "white",
     textColor: "#2c3e50",
-    accentColor: "#0984e3",
-    cardBgColor: "white"
+    accentColor: "#0984e3"
 };
